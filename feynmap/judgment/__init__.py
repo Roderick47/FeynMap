105b8ba4e458b8a42429114ae563b261d84abdd6
Roderick Sasu <38291077+Roderick47@users.noreply.github.com>
2026-09-19 10:44:17 +1000

    Export Context Ranker v1 API

--- feynmap/judgment/__init__.py
+++ feynmap/judgment/__init__.py
@@ -9,6 +9,12 @@ from .contracts import (
     JudgmentResult,
 )
 from .jev import JevJudgmentProvider, JevUnavailableError
+from .ranking import (
+    RankedCandidate,
+    baseline_rank,
+    rerank_with_judgments,
+    rerank_with_judgments_result,
+)
 from .state import build_judgment_state
 
 __all__ = [
@@ -20,5 +26,9 @@ __all__ = [
     "JudgmentProviderError",
     "JudgmentQuestion",
     "JudgmentResult",
+    "RankedCandidate",
+    "baseline_rank",
     "build_judgment_state",
+    "rerank_with_judgments",
+    "rerank_with_judgments_result",
 ]
